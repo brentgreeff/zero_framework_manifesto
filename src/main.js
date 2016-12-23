d09312946c05b44b17a3e7b1cfc75cce80f1d0fd
Brent <brentgreeff@gmail.com>
2016-12-24 03:00:27 +0800

    A few different versions of custom elements, html imports and templates

---
+++ src/main.js
@@ -0,0 +1,2 @@
+require('./util.js');
+require('webcomponents');
---
+++ src/util.js
@@ -0,0 +1,37 @@
+
+Context = function () {
+  var current_script = document.currentScript || document._currentScript
+  if (current_script == null) { throw 'currentScript is null' }
+  this.ownerDoc = current_script.ownerDocument;
+};
+
+Context.prototype.loadTemplate = function (ref) {
+  this.template = this.ownerDoc.querySelector( ref );
+};
+
+Context.prototype.template = function (selector) {
+  if ( selector == null ) { selector = 'template' }
+  this.loadTemplate( selector );
+  return this;
+};
+
+Context.prototype.fragment = function () {
+  return document.importNode( this.template.content, true );
+}
+
+//
+
+NewElement = function(name, context, attr) {
+  var newElemProto = Object.create( HTMLElement.prototype );
+
+  if (context) {
+    newElemProto.createdCallback = function () {
+      this.appendChild( context.template().fragment() );
+    }
+  }
+
+  Object.keys(attr).forEach(function (key) {
+    newElemProto[key] = attr[key];
+  });
+  document.registerElement(name, { prototype: newElemProto });
+}
